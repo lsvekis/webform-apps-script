1d926974c9c75d2b53dc6181acdcef2b2ac177c0
LSvekis <lassvekis@gmail.com>
2022-02-22 20:17:57 -0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,16 +1,19 @@
-const url = '';
+const url = 'https://script.google.com/macros/s/AKfycby4Pw**iR/exec';
 const myForm = document.querySelector('#myForm');
 const myName = document.querySelector('#name');
 const myEmail = document.querySelector('#email');
 const myMessage = document.querySelector('#message');
+const subBtn = document.querySelector('input[type="submit"]');
+const main = document.querySelector('.myForm');
 myName.value = 'Laurence Svekis';
-myEmail.value = 'gapp*****@gmail.com';
+myEmail.value = 'gappscourses+100@gmail.com';
 myMessage.value = 'Hello World';
 myForm.addEventListener('submit', submitter);
 
 function submitter(e) {
     console.log('submitted');
     e.preventDefault();
+    subBtn.disabled = true;
     let message = '';
     if (myName.value.length < 5) {
         myName.style.borderColor = 'red';
@@ -30,12 +33,46 @@ function submitter(e) {
             myName.style.borderColor = '';
             myEmail.style.borderColor = '';
         }, 5000);
+        subBtn.disabled = false;
     } else {
         const myObj = {
             name: myName.value,
             email: myEmail.value,
             message: myMessage.value
         };
-        console.log(myObj);
+        myForm.style.display = 'none';
+        addSendMail(myObj);
     }
+}
+
+function addSendMail(data){
+    console.log(data);
+    const repDiv = document.createElement('div');
+    repDiv.textContent = 'Waiting.....';
+    main.append(repDiv);
+    fetch(url,{
+        method:'POST',
+        body:JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(json =>{
+        console.log(json);
+        if(json.rowval){
+            repDiv.textContent = `Message Sent Your ID is ${json.rowval}`;
+        }else{
+            repDiv.remove();
+            subBtn.disabled = false;
+            myForm.style.display = 'block';
+        }
+        
+    })
+}
+
+function addSendMailGET(data){
+    const url1 = url + '?id=100';
+    fetch(url1)
+    .then(res => res.json())
+    .then(json =>{
+        console.log(json);
+    })
 }
